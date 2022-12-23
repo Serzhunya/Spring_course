6042d28e24638f7c9a8cf70b8e84f662e2065489
Serzhunya <kon-serzhunya@yandex.ru>
2022-12-23 19:34:07 +0300

    update salary

--- src/hibernate_test/Test4.java
+++ src/hibernate_test/Test4.java
@@ -7,16 +7,13 @@ import org.hibernate.cfg.Configuration;
 
 import java.util.List;
 
-public class Test3 {
+public class Test4 {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Employee.class).buildSessionFactory();
         try{
             Session session = factory.getCurrentSession();
             session.beginTransaction();
-            List <Employee> emps = session.createQuery("from Employee").getResultList();
-            for (Employee e: emps) {
-                System.out.println(e);
-            }
+            session.createQuery("UPDATE Employee SET salary = 1500 WHERE name = 'Sara'").executeUpdate();
             session.getTransaction().commit();
         } finally {
             factory.close();
